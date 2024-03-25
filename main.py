0ffdf165be793cf5c2831e37d0b155ded8534c7b
deepikadudala <deepika.dudala@sasi.ac.in>
2024-03-25 20:58:17 +0530

    intial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from typing import Optional
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get('/')
+def main():
+    return "hello"  
+
+@app.get('/blog')
+def blogs(limit:int = 10 ,published:bool = True,sort:Optional[str] = None):
+    if published:
+        return {"data":f'{limit} published blogs'}
+    else:
+        return {"data":f'{limit} unpublished blogs'}
+
+@app.get('/blog/{id}')
+def giveBlog(id:int):
+    return {"data":id}
+
+class Blog(BaseModel):
+    title:str
+    body:str
+    published:Optional[bool]=True
+
+@app.post('/blog')
+def create_blog(request:Blog):
+    return {"data":request}
