b82007e487ba30da3377aab34d7ef941397cfc7a
deepikadudala <deepika.dudala@sasi.ac.in>
2024-03-26 00:32:59 +0530

    sql connection

--- blog/main.py
+++ blog/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from . import models
+from . import schemas
+from .database import engine
+
+
+models.Base.metadata.create_all(engine)
 
 app = FastAPI()
 
 
-class Blog(BaseModel):
-    title:str
-    body:str
-@app.post('/blog')
-def create_blog(request:Blog):
+
+@app.post('/blog') 
+def create_blog(request:schemas.Blog):
     return request
