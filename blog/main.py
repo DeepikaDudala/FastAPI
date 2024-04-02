6c906963286895bf82389f651b93d3e3697aa5f0
sahithidudala <sahithidudala@gmail.com>
2024-04-02 10:54:32 +0530

    post,put, delete,get operations on sql

--- blog/main.py
+++ blog/main.py
@@ -14,7 +14,7 @@ def get_db():
         db.close()
 app=FastAPI()
 
-@app.post('/blog')
+@app.post('/blog',status_code = status.HTTP_201_CREATED,tags=['blogs'])
 def create_blog(request:schemas.Blog,db:Session = Depends(get_db)):
     new_blog  =models.Blog(title = request.title, body = request.body, published = request.published)
     db.add(new_blog)
@@ -22,12 +22,12 @@ def create_blog(request:schemas.Blog,db:Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
-@app.get('/blog')
+@app.get('/blog',tags=['blogs'])
 def get_all_blogs(db:Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}')
+@app.get('/blog/{id}',tags=['blogs'])
 def get_blog(id:int,db:Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -39,15 +39,15 @@ def update_blog(id:int,request:schemas.Blog,db:Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"blog with id {id} not found")
-    blog.update(values={request},synchronize_session=False)
+    blog.update(request.dict()) # type: ignore
     db.commit()
     return "updated"
 
-@app.delete('blog/{id}',status_code=status.HTTP_204_NO_CONTENT,tags=['blogs'])
+@app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT,tags=['blogs'])
 def delete_blog(id:int,db:Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'blog with {id} is not found')
     blog.delete(synchronize_session=False)
-    
+    db.commit()
     return "deleted"
--- blog/schemas.py
+++ blog/schemas.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 class Blog(BaseModel):
     title:str
     body:str
-    published:bool = True
-    class Config():
-        orm_mode = True
+    published:bool = False
+
+class UpdateBlog(BaseModel):
+    published:bool
